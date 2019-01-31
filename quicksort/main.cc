93a3cef957c72d31f33cb7f89ab695fa8c74b800
deeplearningg <1379658566@qq.com>
2019-01-31 21:10:05 +0800

    quicksort

--- quicksort/main.cc
+++ quicksort/main.cc
@@ -1,79 +1,45 @@
-#include<stdio.h>
-#include <vector>
-// A utility function to swap two elements
-void swap(int* a, int* b)
-{
-	int t = *a;
-	*a = *b;
-	*b = t;
-}
-
-/* This function takes last element as pivot, places
-the pivot element at its correct position in sorted
-	array, and places all smaller (smaller than pivot)
-to left of pivot and all greater elements to right
-of pivot */
-int partition (std::vector<int>& arr, int low, int high)
-{
-	int pivot = arr[high]; // pivot
-	int i = (low - 1); // Index of smaller element
-
-	for (int j = low; j <= high- 1; j++)
-	{
-		// If current element is smaller than or
-		// equal to pivot
-		if (arr[j] <= pivot)
-		{
-			i++; // increment index of smaller element
-			swap(&arr[i], &arr[j]);
-            //int temp = arr[j];
-            //arr[j] = arr[i];
-            //arr[i] = temp;
-		}
-	}
-	swap(&arr[i + 1], &arr[high]);
-    //int temp = arr[high];
-    //arr[high] = arr[i+1];
-    //arr[i+1] = temp;
-	return (i + 1);
-}
-
-/* The main function that implements QuickSort
-arr[] --> Array to be sorted,
-low --> Starting index,
-high --> Ending index */
-void quickSort(std::vector<int>& arr, int low, int high)
-{
-	if (low < high)
-	{
-		/* pi is partitioning index, arr[p] is now
-		at right place */
-		int pi = partition(arr, low, high);
-
-		// Separately sort elements before
-		// partition and after partition
-		quickSort(arr, low, pi - 1);
-		quickSort(arr, pi + 1, high);
-	}
-}
-
-/* Function to print an array */
-void printArray(std::vector<int> arr, int size)
-{
-	int i;
-	for (i=0; i < size; i++)
-		printf("%d ", arr[i]);
-	printf("\n");
-}
-
+#include <chrono>  // time
+#include <cstdlib>  // rand
+#include <iostream>
+#include "util.h"
+#include "quicksort.h"
+#include "mergeSort.h"
+#include "IterativeMergeSort.h"
 // Driver program to test above functions
 int main()
 {
-    std::vector<int> arr = {10, 7, 8, 9, 1, 5};
-    int n = arr.size();
-	quickSort(arr, 0, n-1);
-	printf("Sorted array:");
-	printArray(arr, n);
-	return 0;
+    size_t n = 14111;
+    std::vector< int > ret(n);
+    for(int i = 0;i < n;i++)
+        ret[i] = i;
+    std::vector< double > ref(n);
+    srand(0);
+    for ( size_t i = 0; i < n; i++ ) {
+        ref[i] = rand()%(33);
+    }
+
+//    QuickSort qs(ref);
+    MergeSort ms(ref);
+    auto t1 = std::chrono::high_resolution_clock::now();
+#if 1
+// parallel sort
+    //qs.quickSort(ret,0,ret.size()-1);
+    ms.mergeSort(ret, 0, n - 1);
+	//mergeSort(ref, n);
+
+
+#endif
+    auto t2 = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> tms = t2 - t1;
+    std::cout << "Sorted in " << tms.count()/1000.0 << " sec." << std::endl;
+#if 1
+    for(size_t i = 0;i < n;i++){
+        std::cout << ref[ ret[i] ] << "\t";
+        if( (i+1)%8 == 0 )
+            std::cout << std::endl;
+    }
+    std::cout << std::endl;
+#endif
+    return 0;
 }
 
